b5485ac4e8c3ef78aa0f9b5202ccb7212a9f8803
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-12 20:20:34 +0800

    Update REST API Call-No Auth Token.ps1

--- REST API Call-No Auth Token.ps1
+++ REST API Call-No Auth Token.ps1
@@ -19,3 +19,33 @@ $item
 #option 2 for display/utilization
 $response | ConvertTo-Json #-Depth 4
 $response | ConvertTo-Json -Depth 4
+
+
+# v2
+#Query root endpoint
+Invoke-RestMethod -Uri 'https://api.github.com' -Method Get
+
+#Query user endpoint
+Invoke-RestMethod -Uri 'https://api.github.com/users/saritmaitra' -Method Get
+
+# header to request a specific version (v3)
+$header = @{ 'Accept' = 'application/vnd.github.v3+json' } 
+Invoke-RestMethod -Uri 'https://api.github.com' -Method Get -Headers $header
+
+#run without pagination setting
+$repos = Invoke-RestMethod -Uri 'https://api.github.com/users/PowerShell/repos' -Method Get
+$repos | Measure-Object
+
+#run with pagination setting in the URI (1 page with max 100 items)
+$repos = Invoke-RestMethod -Uri 'https://api.github.com/users/PowerShell/repos?page=1&per_page=100' -Method Get
+$repos | Measure-Object
+
+#run specifying pagination setting (1 page with max 100 items)
+$body = @{
+    'page' = 1
+    'per_page' = 50
+}
+
+$body
+$repos = Invoke-RestMethod -Uri 'https://api.github.com/users/PowerShell/repos' -Method Get -Body $body
+$repos | Measure-Object
