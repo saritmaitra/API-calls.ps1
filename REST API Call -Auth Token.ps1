1be52201cb50660a1c1c21c4e3b8af30058ff3f8
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-12 20:44:02 +0800

    Update REST API Call -Auth Token.ps1

--- REST API Call -Auth Token.ps1
+++ REST API Call -Auth Token.ps1
@@ -12,3 +12,28 @@ $header = @{authorization = "Basic $token"}
 $response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header
 
 $response | ConvertTo-Json -Depth 4
+
+#v2
+# With authentication
+Invoke-RestMethod -Uri 'https://api.github.com/users/orgs' -Method Get -Headers $header
+$error[0].Exception
+$error[0].ErrorDetails.Message | ConvertFrom-Json
+
+# window powershell 5.1
+$header = @{
+    'Authorization' = 'token xxxxxxxxxxx'
+}
+Invoke-RestMethod -Uri 'https://api.github.com/users/orgs' -Method Get -Headers $header
+
+#powershell core
+$token = Read-Host -Prompt 'API Token' -AsSecureString
+Invoke-RestMethod -Uri 'https://api.github.com/users/orgs' -Method Get -Authentication OAuth -Token $token
+
+# Auto de-serialization
+
+# non-powershell returning row json
+$curlOutput = curl 'https://api.github.com'
+$curlOutput.Content
+
+# powershell de-serialization the json automatically into PSCustomeObject
+(Invoke-RestMethod -Uri 'https://api.github.com' -Method Get).GetType()
