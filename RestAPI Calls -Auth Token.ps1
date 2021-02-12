27ab7e601833160f7d3ddb36cc83e979183ef02d
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-12 19:12:06 +0800

    Create RestAPI Calls -Auth Token.ps1

---
+++ RestAPI Calls -Auth Token.ps1
@@ -0,0 +1,14 @@
+#https://docs.microsoft.com/en-us/rest/api/azure/devops/distributedtask/variablegroups/get%20variable%20groups?view=azure-devops-rest-5.1
+
+$url_base = "MY_BASE_ENDPOINT_URL"
+$url_endpoint = "MY_ENDPOINT"
+$url = $url_base + $url_endpoint
+$Personal_Access_Token = "MY_ACCESS_TOKEN"
+$user = ""
+
+$token = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $Personal_Access_Token)))
+$header = @{authorization = "Basic $token"}
+
+$response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header
+
+$response | ConvertTo-Json -Depth 4
